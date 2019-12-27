b82fbe62e55095940aa059459c40a588a69540ff
Martin Veith <ndresx@gmail.com>
2019-12-27 15:30:04 +0100

    Re-add index

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+export {
+  zeroPad,
+  calcTimeDelta,
+  formatTimeDelta,
+  CountdownTimeDeltaOptions,
+  CountdownTimeDelta,
+  CountdownTimeDeltaFormatted,
+  CountdownTimeDeltaFormatOptions,
+} from './utils';
+
+export {
+  CountdownProps,
+  CountdownRenderProps,
+  CountdownApi,
+  CountdownTimeDeltaFn,
+} from './CountdownJs';
+import Countdown from './Countdown';
+export default Countdown;
+
+import useCountdown, { UseCountdownProps, UseCountdownResult } from './useCountdown';
+export { useCountdown, UseCountdownProps, UseCountdownResult };
